c45dc7ad02d6e887fd12fa6b7d3f55a6863ff345
Abul Bashar <absbashar04@gmail.com>
2026-08-14 18:34:28 +0600

    next public added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
 export const config = {
-  base_url: process.env.BASE_URL,
-  node_env: process.env.NODE_ENV,
+  base_url: process.env.NEXT_PUBLIC_BASE_URL,
+  node_env: process.env.NEXT_PUBLIC_NODE_ENV,
 };
